8f33bf41d6b22b653458d236dcde329b2bcadda9
John Cooley <john.cooley@manchester.ac.uk>
2020-08-19 14:59:33 +0100

    Add project files.

---
+++ Models/Category.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace WebApp.Models
+{
+    public class Category
+    {
+        public long CategoryId { get; set; }
+        public string Name { get; set; }
+        public IEnumerable<Product> Products { get; set; }
+    }
+}
---
+++ TestMiddleware.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApp.Models;
+
+namespace WebApp
+{
+    public class TestMiddleware
+    {
+        private RequestDelegate nextDelegate;
+
+        public TestMiddleware(RequestDelegate next)
+        {
+            nextDelegate = next;
+        }
+
+        public async Task Invoke(HttpContext context, DataContext dataContext)
+        {
+            if (context.Request.Path == "/test")
+            {
+                await context.Response.WriteAsync($"There are {dataContext.Products.Count()} Products\n");
+                await context.Response.WriteAsync($"There are {dataContext.Categories.Count()} Categories\n");
+                await context.Response.WriteAsync($"There are {dataContext.Suppliers.Count()} suppliers\n");
+            }
+            else
+            {
+                await nextDelegate(context);
+            }
+        }
+    }
+}
